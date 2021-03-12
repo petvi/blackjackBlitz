ab058d589d80225f9ae050dfa0edec157ef5e01b
John Doe <petrasvilkelis@gmail.com>
2021-03-12 19:07:37 +0100

    message

---
+++ app/questions/components/QuestionForm.tsx
@@ -0,0 +1,15 @@
+import { Form, FormProps } from "app/core/components/Form"
+import { LabeledTextField } from "app/core/components/LabeledTextField"
+import * as z from "zod"
+export { FORM_ERROR } from "app/core/components/Form"
+
+export function QuestionForm<S extends z.ZodType<any, any>>(props: FormProps<S>) {
+  return (
+    <Form<S> {...props}>
+      <LabeledTextField name="text" label="Name" placeholder="Name" />
+      <LabeledTextField name="choices.0.text" label="Choice 1" placeholder="Choice 1" />
+      <LabeledTextField name="choices.1.text" label="Choice 2" placeholder="Choice 2" />
+      <LabeledTextField name="choices.2.text" label="Choice 3" placeholder="Choice 3" />
+    </Form>
+  )
+}
---
+++ app/questions/mutations/updateQuestion.ts
@@ -0,0 +1,34 @@
+import { resolver } from "blitz"
+import db from "db"
+import * as z from "zod"
+
+const UpdateQuestion = z
+  .object({
+    id: z.number(),
+    text: z.string(),
+    choices: z.array(z.object({ id: z.number().optional(), text: z.string() }).nonstrict()),
+  })
+  .nonstrict()
+
+export default resolver.pipe(
+  resolver.zod(UpdateQuestion),
+  resolver.authorize(),
+  async ({ id, ...data }) => {
+    // TODO: in multi-tenant app, you must add validation to ensure correct tenant
+    const question = await db.question.update({
+      where: { id },
+      data: {
+        ...data,
+        choices: {
+          upsert: data.choices.map((choice) => ({
+            where: { id: choice.id },
+            create: { text: choice.text },
+            update: { text: choice.text },
+          })),
+        },
+      },
+    })
+
+    return question
+  }
+)
